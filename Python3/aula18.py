a28c3eaf81b60a46b5f6c33f96159ff91b0c1291
Gabriel Barbosa Souza <gabrielbarbosasouzabiel@gmail.com>
2026-02-28 16:42:20 -0300

    Desafio84

--- Python3/aula18.py
+++ Python3/aula18.py
@@ -1,21 +1,31 @@
-pessoas = list()
+def desafio84():
+    pessoas = list()
 
-while True:
-    nome = str(input('Digite um nome: '))
-    peso = float(input('Digite uma peso: '))
-    
-    pessoas.append([nome, peso])
-    
-    continuar = str(input('Deseja continuar? (S/N): ')).strip().lower()
-    if continuar == 'n':
-        break
-    elif continuar != 's':
-        continuar = str(input('Digite "S" para sim e "N" para não')).strip().lower()
+    while True:
+        nome = str(input('Digite um nome: '))
+        peso = float(input('Digite uma peso: '))
+        
+        pessoas.append([nome, peso])
+        
+        continuar = str(input('Deseja continuar? (S/N): ')).strip().lower()
+        if continuar == 'n':
+            break
+        elif continuar != 's':
+            continuar = str(input('Digite "S" para sim e "N" para não')).strip().lower()
 
-kg = [p[1] for p in pessoas]
-kg.sort()
+    kg = [p[1] for p in pessoas]
+    pesado = max(kg)
+    leve = min(kg)
 
-print(kg)
-print(f'Foram cadastradas {len(pessoas)} pessoas.')
-print(f'{kg[0:]} são as pessoas mais pesadas.')
-print(f'{} são as pessoas mais leves.')
+    print(f'Foram cadastradas {len(pessoas)} pessoas.')
+
+    print(f'A pessoa mais pesada é ', end='')
+    for p in pessoas:
+        if p[1] == pesado:
+            print(p[0])
+            
+    print(f'A pessoa mais leve é ', end='')
+    for p in pessoas:
+        if p[1] == leve:
+            print(p[0])
+            
